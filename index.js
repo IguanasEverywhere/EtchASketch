6f409fde8f0e74484e9dde2cb4417f1d14d7ab61
Scott Schwab <schwabpiano@gmail.com>
2021-12-20 14:33:59 -0600

    Render container with specific number of squares across

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-alert("Hi");
+
+
+let size = prompt("How many boxes across?");
+
+
+
+
+
+    for (let i = 0; i < (size * size); i++) {
+        let box = document.createElement("div");
+        box.className = "box";
+        box.style.width = (500/size) - (2) + "px";
+        document.getElementById("boxes-container").appendChild(box);
+    }
